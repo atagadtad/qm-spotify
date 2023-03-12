0acb586af96dd593fa6eb4d3045a42c7d8c35e6b
Aaron Tagadtad <aaron@alias.com>
2023-03-12 18:50:59 -0400

    fetch request

---
+++ pages/_app.js
@@ -0,0 +1,6 @@
+import { useEffect } from "react";
+import "../styles/globals.css";
+
+export default function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,25 @@
-export default function Home() {
+import { useEffect } from "react";
+
+const Hello = () => {
+  useEffect(() => {
+    const fetchCocktail = async (cocktail) => {
+      const result = await fetch(
+        `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${cocktail}`
+      );
+      const data = await result.json();
+      console.log({ data });
+    };
+
+    fetchCocktail("martini");
+  }, []);
+
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    <div className="flex">
+      <div className="mx-auto">
+        <h1 className="text-3xl font-bold underline">Cocktails</h1>
+      </div>
     </div>
   );
-}
+};
+
+export default Hello;
